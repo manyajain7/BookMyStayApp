50fc2c5ec611ed4aa2b0f77debebadebf61fadea
manyajain7 <manyajain2402@gmail.com>
2026-03-17 15:34:22 +0530

    UC12 is done

--- app/src/BookMyStayApp.java
+++ app/src/BookMyStayApp.java
@@ -1,12 +1,5 @@
-import java.util.*;
-
 public class BookMyStayApp {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("1.View 2.Exit");
-        int ch=sc.nextInt();
-
-        if(ch==1) System.out.println("Viewing rooms");
-        else System.out.println("Exit");
+        System.out.println("Hotel Booking System Running Successfully!");
     }
 }
